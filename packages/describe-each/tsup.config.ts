ac15ba899eb477e77a52b193aefc9e9cc4bd93aa
William Wong <compulim@hotmail.com>
2026-07-22 00:09:48 -0700

    Rename entrypoint

--- packages/describe-each/tsup.config.ts
+++ packages/describe-each/tsup.config.ts
@@ -4,7 +4,7 @@ import overrideConfig from './tsup.config.override.ts';
 const baseConfig: Options = {
   dts: true,
   entry: {
-    'describe-each': './src/index.ts'
+    index: './src/index.ts'
   },
   sourcemap: true
 };
